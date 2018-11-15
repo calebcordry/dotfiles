33063edeabdc93091c0f4268b8157d1bed9f7c3b
calebcordry <ccordry@google.com>
2018-11-14 20:33:26 -0800

    modify install script

--- install.sh
+++ install.sh
@@ -6,7 +6,14 @@ realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
-Names=(".gitconfig" ".gitmodules" ".vim" ".vimrc" ".zshrc")
+# List of files to be linked.
+Names=(
+  ".gitconfig" 
+  ".gitmodules" 
+  ".vim" 
+  ".vimrc" 
+  ".zshrc"
+)
 
 # Get root
 SCRIPT_PATH=$(realpath $0)
@@ -15,8 +22,5 @@ DOTFILES=$(dirname $SCRIPT_PATH)
 # Install dotfiles
 for f in "${Names[@]}"; do
     echo "linking $DOTFILES/$f to $HOME"
-    ln -fsn "$DOTFILES/$FILE" "$HOME"
-echo $f
+    ln -fsh "$DOTFILES/$f" "$HOME"
 done
-
-git submodule init && git submodule update
